19a6519ce30ffa48dad6ac779be980a7f5f7f5c1
MilanTheGamer <milanthegamer@gmail.com>
2020-10-20 21:52:05 +0530

    Updates

---
+++ precache-manifest.2f065a9104ce50b64764ba908c2bce2a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747ebc7d2fede2c0ee49ab0e4271af7e",
+    "url": "/munnasGroup/index.html"
+  },
+  {
+    "revision": "a21dbb35a597d84b3294",
+    "url": "/munnasGroup/static/css/main.e1c8f8ea.chunk.css"
+  },
+  {
+    "revision": "8d20bebb712c17addddc",
+    "url": "/munnasGroup/static/js/2.ffc91653.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/munnasGroup/static/js/2.ffc91653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21dbb35a597d84b3294",
+    "url": "/munnasGroup/static/js/main.3a80317e.chunk.js"
+  },
+  {
+    "revision": "ed711679bdd70d4d3fda",
+    "url": "/munnasGroup/static/js/runtime-main.33a42e1a.js"
+  },
+  {
+    "revision": "84fa84de4e7b75670dcc68abb1deced3",
+    "url": "/munnasGroup/static/media/callIcon.84fa84de.svg"
+  },
+  {
+    "revision": "4ed0cd61010e24524b878d7906e6abad",
+    "url": "/munnasGroup/static/media/d4.4ed0cd61.jpg"
+  },
+  {
+    "revision": "82bd35e1ced062031abe2cace925f59e",
+    "url": "/munnasGroup/static/media/distributions.82bd35e1.jpg"
+  },
+  {
+    "revision": "a93708edc9eabb3301c61abe4eaafbb0",
+    "url": "/munnasGroup/static/media/down_arrow_white.a93708ed.svg"
+  },
+  {
+    "revision": "3e10de0e8a9be28a8b13bab5cf110aae",
+    "url": "/munnasGroup/static/media/emailIcon.3e10de0e.svg"
+  },
+  {
+    "revision": "57108f3bb56f8702bbf224faadb0a362",
+    "url": "/munnasGroup/static/media/facebookIcon.57108f3b.svg"
+  },
+  {
+    "revision": "04d4681fd029b42cfe1f06afda17255b",
+    "url": "/munnasGroup/static/media/home1.04d4681f.jpg"
+  },
+  {
+    "revision": "71a0dad88dada29779556b7d9000d7f0",
+    "url": "/munnasGroup/static/media/home7.71a0dad8.jpg"
+  },
+  {
+    "revision": "ebd588539a429a7a62559030aea22976",
+    "url": "/munnasGroup/static/media/locationIcon.ebd58853.svg"
+  },
+  {
+    "revision": "79950137513179b08414cb10e4acce90",
+    "url": "/munnasGroup/static/media/logo.79950137.png"
+  },
+  {
+    "revision": "03824bc6123345fc8cb24766a8b6d527",
+    "url": "/munnasGroup/static/media/mapLocation.03824bc6.png"
+  },
+  {
+    "revision": "42bd2cb6775a36e7d7ae2bd196405785",
+    "url": "/munnasGroup/static/media/menu.42bd2cb6.svg"
+  },
+  {
+    "revision": "99378d80fbd4fd611eb5008ffea0ffa5",
+    "url": "/munnasGroup/static/media/restaurant1.99378d80.jpg"
+  },
+  {
+    "revision": "ab4e27aa873c6e7f181b637f1941c08d",
+    "url": "/munnasGroup/static/media/restrauntHome.ab4e27aa.jpg"
+  },
+  {
+    "revision": "2fd19e064fd9e87411f4146a97fc6589",
+    "url": "/munnasGroup/static/media/story.2fd19e06.png"
+  },
+  {
+    "revision": "db2356057dcabfd24db69c33ff8f02ed",
+    "url": "/munnasGroup/static/media/supermarket2.db235605.jpg"
+  },
+  {
+    "revision": "5f1bc9080ece962d6969c651301e6cae",
+    "url": "/munnasGroup/static/media/supermarketMain.5f1bc908.jpg"
+  },
+  {
+    "revision": "bbf536e8757792e6d40b1b9f1557b4fb",
+    "url": "/munnasGroup/static/media/y2.bbf536e8.jpg"
+  }
+]);
